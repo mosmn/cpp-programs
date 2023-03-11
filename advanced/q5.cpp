71bc0c31263f940e7eba9a71647591eb158eba11
mosmn <ithinkisee8@gmail.com>
2023-03-11 16:48:09 +0800

    J

--- advanced/q5.cpp
+++ advanced/q5.cpp
@@ -2,17 +2,17 @@
 #include <math.h>
 using namespace std;
 
-inline double circleArea (double x) {
-    return M_PI * x * x;
+inline double circleArea (double radius) {
+    return M_PI * radius * radius;
 }
 
 int main()
 {
     double radius = 0.0, area = 0.0;
-    cout << "enter radius: ";
+    cout << "Enter radius: ";
     cin >> radius;
     
     area = circleArea(radius);
-    cout << "\narea is: " << area;
+    cout << "\nArea is: " << area;
     return 0;
 }
