f28136b522d059af9b1d7f0f480b70b2b19dd9bc
mosmn <ithinkisee8@gmail.com>
2023-05-27 06:31:24 +0800

    h

--- main.cpp
+++ main.cpp
@@ -3,12 +3,23 @@
 
 using namespace std;
 
-int main()
-{
-    double a = 3.456;
+double GetWeeklyHours (void) {
+    double h = 46.50;
+    return h;
+}
 
-    cout << setw(10) << setfill('@') << a << endl;
-    cout << fixed << setprecision(2)<< left << setw(10) << setfill('*') << a << endl;
-    cout  << setprecision(1) << right << setw(15) << setfill('-') << a << endl;
+double GetSalary(void) {
+    double salary = 26.48;
+    return salary;
+}
+
+int main(){
+    double hours, salary, WeeklySalary;
+    hours = GetWeeklyHours();
+    salary = GetSalary();
+    cout << "Weekly Hours: " << hours << '\n';
+    cout<<"Hourly Salary: " << salary << '\n';
+    WeeklySalary = hours * salary;
+    cout << "Weekly Salary: " << WeeklySalary << '\n';
     return 0;
 }
